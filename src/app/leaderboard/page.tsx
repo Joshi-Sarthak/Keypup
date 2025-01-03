99b5ec5fedc5a20590b7d7e34b4cedffa25e2f63
AtharvaBhore <atharvabhore6@gmail.com>
2025-01-03 21:47:36 +0530

    Leaderboard UI changes

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { useEffect, useState } from "react"
-import { VscDebugRestart } from "react-icons/vsc"
+import { PiMedalFill } from "react-icons/pi"
 import { FaA } from "react-icons/fa6"
 import { FaRegClock } from "react-icons/fa"
 import { BiSolidQuoteAltLeft } from "react-icons/bi"
@@ -20,7 +20,6 @@ interface LeaderboardMode {
 
 export default function Page() {
 	const [leaderboard, setLeaderboard] = useState<LeaderboardMode[]>([])
-	const [isHovered, setIsHovered] = useState(false)
 	const [selectedMode, setSelectedMode] = useState<string>("words")
 
 	useEffect(() => {
@@ -55,7 +54,7 @@ export default function Page() {
 
 	return (
 		<div className="flex flex-col items-center w-full mt-3">
-			<div className="text-2xl font-semibold tracking-widest text-stone-400 dark:text-neutral-500 text-center mt-0 mb-1">
+			<div className="tracking-widest text-3xl my-7 text-stone-500 dark:text-neutral-500 text-center mt-0 mb-1">
 				Leaderboard
 			</div>
 			{/* Mode Selector */}
@@ -105,18 +104,19 @@ export default function Page() {
 			{leaderboard.length === 0 ? (
 				<div className="text-center text-xl text-stone-500">Loading...</div>
 			) : (
-				<div className="w-4/5 flex justify-center gap-8">
+				<div className="w-3/4 flex justify-center gap-8">
 					{/* Render only the selected mode */}
 					<div key={selectedMode} className="w-full flex flex-col">
-						<div className="w-full border border-stone-300 dark:border-neutral-600 rounded-lg p-4">
+						<div className="w-full border border-stone-300 dark:border-neutral-600 rounded-3xl py-1 px-4 bg-white dark:bg-[#242120]">
 							<div
-								className="grid grid-cols-4 text-lg font-medium text-neutral-300 dark:text-stone-400 mb-2 text-center"
+								className="grid grid-cols-5 text-lg font-medium pt-1 pb-3 border-b border-stone-200 dark:border-neutral-700 text-neutral-300 dark:text-stone-400 text-center bg-white dark:bg-[#242120]"
 								style={{
-									color:"#7e22ce",
+									color: "#7e22ce",
 								}}
 							>
-								<span className="px-2 ">Player</span>
-								<span className="px-2">SubType</span>
+								<span className="px-2">Rank</span>
+								<span className="px-2">Player</span>
+								<span className="px-2">Subtype</span>
 								<span className="px-2">Raw WPM</span>
 								<span className="px-2">WPM</span>
 							</div>
@@ -126,22 +126,33 @@ export default function Page() {
 									(result, idx) => (
 										<div
 											key={idx}
-											className={`grid grid-cols-4 text-base py-2.5 px-3 border-b last:border-b-0 border-stone-200 dark:border-neutral-700 text-center ${
+											className={`grid grid-cols-5 text-base py-2.5 border-b last:border-b-0 border-stone-200 dark:border-neutral-700 text-center text-stone-600 dark:text-stone-400 ${
 												idx % 2 === 0
-													? "text-stone-600 dark:text-stone-400"
-													: " text-stone-950 dark:text-stone-500"
+													? "bg-stone-800"
+													: "bg-white dark:bg-[#242120]"
 											}`}
 										>
-											<span className="px-2">
+											<span className="ml-2">
+												{idx == 0 ? (
+													<PiMedalFill className="text-yellow-400 ml-32" />
+												) : idx == 1 ? (
+													<PiMedalFill className="text-gray-300 ml-32" />
+												) : idx == 2 ? (
+													<PiMedalFill className="text-[#CD7F32] ml-32" />
+												) : (
+													idx + 1
+												)}
+											</span>
+											<span className="px-4">
 												{result.playerName}
 											</span>
-											<span className="px-2">
+											<span className="px-4">
 												{result.subType}
 											</span>
-											<span className="px-2">
+											<span className="px-4">
 												{result.rawSpeed}
 											</span>
-											<span className="px-2">{result.wpm}</span>
+											<span className="px-4">{result.wpm}</span>
 										</div>
 									)
 								)
@@ -154,21 +165,6 @@ export default function Page() {
 					</div>
 				</div>
 			)}
-			<div
-				onClick={() => window.location.reload()}
-				onMouseEnter={() => setIsHovered(true)}
-				onMouseLeave={() => setIsHovered(false)}
-				className="flex flex-col items-center cursor-pointer mt-8"
-			>
-				<VscDebugRestart className="w-8 h-8 text-stone-400 dark:text-neutral-600 hover:text-stone-500 hover:dark:text-neutral-500 transition-all duration-200 ease-in-out"></VscDebugRestart>
-				<div
-					className={`text-center text-xs tracking-widest text-stone-500 dark:text-neutral-500 transition-opacity duration-300 ease-in-out ${
-						isHovered ? "opacity-100" : "opacity-0 pointer-events-none"
-					}`}
-				>
-					Reload Leaderboard
-				</div>
-			</div>
 		</div>
 	)
 }
