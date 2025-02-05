4fcdc653d772b6da74fc112c4b0a5e74d503b73f
AtharvaBhore <atharvabhore6@gmail.com>
2025-02-05 15:28:46 +0530

    Fix multiplayer to home page navigation bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 "use client"
 
+import { useEffect } from "react"
 import Controlbar from "@/components/Controlbar/Controlbar"
 import Main from "@/components/Main/Main"
 import Result from "@/components/Result/Result"
 import { useTestStore } from "@/lib/zustand/teststore"
+import { useMultiplayerstore } from "@/lib/zustand/multiplayerstore"
 
 export default function Home() {
 	const loadResult = useTestStore((state) => state.loadResult)
+	const returningFromMultiplayerResult = useMultiplayerstore(
+		(state) => state.inResult
+	)
+
+	const resetTestStore = useTestStore((state) => state.reset)
+
+	if (!loadResult && returningFromMultiplayerResult) {
+		resetTestStore()
+	}
 
 	return loadResult ? (
 		<Result />
